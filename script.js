01bb09c89646c8c80082013e6c0968a4938db9f5
Denex A Poulose <107526656+denex-a-poulose@users.noreply.github.com>
2022-09-08 15:09:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ icon.onclick = function() {
 
 
   if (document.body.classList.contains('dark-theme')) {
-    icon.style.color = "#e9e5f5";
+    icon.style.color = "#9450f2";
     icon.style.transform = "rotateZ(180deg)"
     icon.style.transition = "all ease 1s";
     elements.style.transition = 'all ease 1s';
